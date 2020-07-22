ee99d99d18b0d3798119f6d0e5592179c73f2156
sneha zanzane <sneha.zanzane@gmail.com>
2020-07-22 14:57:00 +0530

    Completed : 1. User list item designed 2. Article list item designed

--- app/src/main/java/com/assignment/articleassignment/view/activity/MainActivity.kt
+++ app/src/main/java/com/assignment/articleassignment/view/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package com.assignment.articleassignment.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.assignment.articleassignment.R
+import kotlinx.android.synthetic.main.view_article_item.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
